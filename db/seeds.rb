14f8ee1b5af52710951f22d3cf31cfc01ed1c323
rinaS <158763752+ririnanaa@users.noreply.github.com>
2024-05-05 08:30:49 +0000

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-   email: 'webcamp@gmail.com',
-   password: 'precure'
-   )
+
